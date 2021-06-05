7c5a242c597a880bfea2b54917eda15297de83d6
arsath1998 <47441409+arsath1998@users.noreply.github.com>
2021-06-06 00:04:26 +0530

    Add files via upload

---
+++ Firstdemo.java
@@ -0,0 +1,14 @@
+package hit.day4;
+
+public class Firstdemo {
+
+	public static void main(String[] args) {
+		System.out.println("Addition of two numbers ....");
+
+		int x=10;
+		int y=20;
+		
+		System.out.println("The addition of two numbers x and y is..."+(x+y));
+	}
+
+}
